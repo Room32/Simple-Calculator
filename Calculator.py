c322c227131541142da75c1661b755c9092ed585
Room32 <domojdoroga@gmail.com>
2022-10-03 20:32:22 +0700

    Finnaly done calculator
    Add exception and fix bugs. Everything is work. TKinter library

--- Calculator.py
+++ Calculator.py
@@ -1,32 +1,101 @@
 import tkinter as tk
+from tkinter import messagebox
+
 
 def add_digit(digit):
-    value = type_text.get() + str(digit)
+    value = type_text.get()
+    if value[0] == '0' and len(value) == 1:
+        value = value[1:]
     type_text.delete(0, tk.END)
-    type_text.insert(0, value)
+    type_text.insert(0, value+digit)
+
+def add_operation(operation):
+    value = type_text.get()
+    if value[-1] in '+-/*':
+        value = value[:-1]
+    elif '+' in value or '-' in value or '*' in value or '/' in value:
+        calculate()
+        value = type_text.get()
+    type_text.delete(0, tk.END)
+    type_text.insert(0, value+operation)
+
+def calculate():
+    value = type_text.get()
+    if value[-1] in '+-/*':
+        value = value + value[:-1]
+    type_text.delete(0, tk.END)
+    try:
+        type_text.insert(0, eval(value))
+    except (NameError, SyntaxError):
+        messagebox.showinfo('ВНИМАНИЕ', 'Вы ввели не цифры')
+        type_text.insert(0,0)
+    except ZeroDivisionError:
+        messagebox.showinfo('ВНИМАНИЕ', 'На ноль делить нельзя!')
+        type_text.insert(0, 0)
+
+
+def clear():
+    type_text.delete(0, tk.END)
+    type_text.insert(0, 0)
+
+def make_digit_button(digit):
+    return tk.Button(text=digit, bd=5, font=('Arial', 13), command=lambda: add_digit(digit))
+
+def make_operation_button(operation):
+    return tk.Button(text=operation, bd=5, font=('Arial', 13), fg='red',
+                     command=lambda: add_operation(operation))
+
+def make_calk_button(operation):
+    return tk.Button(text=operation, bd=5, font=('Arial', 13), fg='red',
+                     command=calculate)
+
+def make_clear_button(operation):
+    return tk.Button(text=operation, bd=5, font=('Arial', 13), fg='red',
+                     command=clear)
+
+def press_key(event):
+    print(event.char)
+    if event.char.isdigit():
+        add_digit(event.char)
+    elif event.char in '+-*/':
+        add_operation(event.char)
+    elif event.char == '\r':
+        calculate()
 
 win = tk.Tk()
-win.geometry(f'240x250+100+200')
+win.geometry(f'240x280+100+200')
 win['bg'] = '#9C9595'
 win.title('Calculator')
 
-type_text = tk.Entry(win, justify=tk.RIGHT, font=('Arial', 15), width=15)
-type_text.grid(row=0, column=0, columnspan=3, sticky='we')
+win.bind('<Key>', press_key)
 
-tk.Button(text='1',bd=5,font=('Arial',13),command=lambda : add_digit(1)).grid(row=1,column=0,stick='wens',padx=5,pady=5)
-tk.Button(text='2',bd=5,font=('Arial',13),command=lambda : add_digit(2)).grid(row=1,column=1,stick='wens',padx=5,pady=5)
-tk.Button(text='3',bd=5,font=('Arial',13),command=lambda : add_digit(3)).grid(row=1,column=2,stick='wens',padx=5,pady=5)
-tk.Button(text='4',bd=5,font=('Arial',13),command=lambda : add_digit(4)).grid(row=2,column=0,stick='wens',padx=5,pady=5)
-tk.Button(text='5',bd=5,font=('Arial',13),command=lambda : add_digit(5)).grid(row=2,column=1,stick='wens',padx=5,pady=5)
-tk.Button(text='6',bd=5,font=('Arial',13),command=lambda : add_digit(6)).grid(row=2,column=2,stick='wens',padx=5,pady=5)
-tk.Button(text='7',bd=5,font=('Arial',13),command=lambda : add_digit(7)).grid(row=3,column=0,stick='wens',padx=5,pady=5)
-tk.Button(text='8',bd=5,font=('Arial',13),command=lambda : add_digit(8)).grid(row=3,column=1,stick='wens',padx=5,pady=5)
-tk.Button(text='9',bd=5,font=('Arial',13),command=lambda : add_digit(9)).grid(row=3,column=2,stick='wens',padx=5,pady=5)
-tk.Button(text='0',bd=5,font=('Arial',13),command=lambda : add_digit(0)).grid(row=4,column=0,stick='wens',padx=5,pady=5)
+type_text = tk.Entry(win, justify=tk.RIGHT, font=('Arial', 15), width=15)
+type_text.insert(0,'0')
+type_text.grid(row=0, column=0, columnspan=4, sticky='we', padx=5, pady=5)
+
+make_digit_button('1').grid(row=1, column=0, stick='wens', padx=5, pady=5)
+make_digit_button('2').grid(row=1, column=1, stick='wens', padx=5, pady=5)
+make_digit_button('3').grid(row=1, column=2, stick='wens', padx=5, pady=5)
+make_digit_button('4').grid(row=2, column=0, stick='wens', padx=5, pady=5)
+make_digit_button('5').grid(row=2, column=1, stick='wens', padx=5, pady=5)
+make_digit_button('6').grid(row=2, column=2, stick='wens', padx=5, pady=5)
+make_digit_button('7').grid(row=3, column=0, stick='wens', padx=5, pady=5)
+make_digit_button('8').grid(row=3, column=1, stick='wens', padx=5, pady=5)
+make_digit_button('9').grid(row=3, column=2, stick='wens', padx=5, pady=5)
+make_digit_button('0').grid(row=4, column=0, stick='wens', padx=5, pady=5)
+
+make_operation_button('+').grid(row=1, column=3, stick='wens', padx=5, pady=5)
+make_operation_button('-').grid(row=2, column=3, stick='wens', padx=5, pady=5)
+make_operation_button('/').grid(row=3, column=3, stick='wens', padx=5, pady=5)
+make_operation_button('*').grid(row=4, column=3, stick='wens', padx=5, pady=5)
+
+make_calk_button('=').grid(row=4, column=2, stick='wens', padx=5, pady=5)
+make_clear_button('C').grid(row=4, column=1, stick='wens', padx=5, pady=5)
 
 win.grid_columnconfigure(0, minsize=60)
 win.grid_columnconfigure(1, minsize=60)
 win.grid_columnconfigure(2, minsize=60)
+win.grid_columnconfigure(3, minsize=60)
 
 win.grid_rowconfigure(1, minsize=60)
 win.grid_rowconfigure(2, minsize=60)
